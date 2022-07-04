758d598ecfc3ad7503fcf225e5fde9a70c5f81f1
andrew-jp <74275670+andrew-jp@users.noreply.github.com>
2022-07-03 21:09:15 -0600

    initial

---
+++ Lab02/Account.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Lab02
+{
+    /// <summary>
+    /// Bank account class - uses IAccount interface
+    /// Instantiate with IAccount [name] = new Account()
+    /// </summary>
+    public class Account : IAccount
+    {
+        /// <summary>
+        /// Minimum account balance
+        /// </summary>
+        private const decimal MIN_BAL = 100.00m;
+
+        /// <summary>
+        /// Account holder name
+        /// </summary>
+        private string name = "";
+
+        /// <summary>
+        /// Account holder address
+        /// </summary>
+        private string address = "";
+
+        /// <summary>
+        /// Account balance (decimal value)
+        /// </summary>
+        private decimal balance;
+
+        /// <summary>
+        /// Account number
+        /// </summary>
+        private int account_number;
+
+        /// <summary>
+        /// Current account state -
+        /// defualt to new_account upon creation
+        /// </summary>
+        private Enum state = AccountState.new_account;
+
+        /// <summary>
+        /// enumerator to hold possible account states
+        /// </summary>
+        private enum AccountState {
+            new_account,
+            active,
+            under_audit,
+            frozen,
+            closed
+        }
+
+        /// <summary>
+        /// Sets account holder name
+        /// </summary>
+        /// <param name="inName"></param>
+        /// <returns>bool</returns>
+        public bool SetName(string inName)
+        {
+            name = inName;
+            if (inName == null || inName == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Get account holder name
+        /// </summary>
+        /// <returns>string</returns>
+        public string GetName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Set account holder address 
+        /// </summary>
+        /// <param name="inAddress"></param>
+        /// <returns>bool</returns>
+        public bool SetAddress(string inAddress)
+        {
+            address = inAddress;
+            if (inAddress == null || inAddress == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Return account holder address
+        /// </summary>
+        /// <returns>string</returns>
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        /// <summary>
+        /// Add funds to account - must be positive
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>bool</returns>
+        public bool PayInFunds(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                return true;
+            }
+            return false;
+            
+        }
+
+        /// <summary>
+        /// Withdraw funds from account - cannot exceed account balance
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>bool</returns>
+        public bool WithdrawFunds(decimal amount)
+        {
+            if (amount > balance)
+            {
+                return false;
+            }
+
+            balance -= amount;
+            return true;
+        }
+
+        /// <summary>
+        /// Set account balance - must be greater than minimum balance
+        /// </summary>
+        /// <param name="inBalance"></param>
+        /// <returns>bool</returns>
+        public bool SetBalance(decimal inBalance)
+        {
+            if (inBalance < MIN_BAL)
+            {
+                return false;
+            }
+
+            balance = inBalance;
+            return true;
+        }
+
+        /// <summary>
+        /// Get account balance
+        /// </summary>
+        /// <returns>decimal</returns>
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        /// <summary>
+        /// Set account number
+        /// </summary>
+        /// <param name="inAccountNum"></param>
+        /// <returns>bool</returns>
+        public bool SetAccountNumber(int inAccountNum)
+        {
+            if (inAccountNum == 0 || inAccountNum < 0)
+            {
+                return false;
+            }
+
+            account_number = inAccountNum;
+            return true;
+        }
+
+        /// <summary>
+        /// Get account number
+        /// </summary>
+        /// <returns>int</returns>
+        public int GetAccountNumber()
+        {
+            return account_number;
+        }
+
+        /// <summary>
+        /// Set account state
+        /// </summary>
+        /// <param name="inState"></param>
+        /// <returns>bool</returns>
+        public bool SetState(string inState)
+        {
+            switch (inState)
+            {
+                case "new_account":
+                    state = AccountState.new_account;
+                    return true;
+                case "active":
+                    state = AccountState.active;
+                    return true;
+                case "under_audit":
+                    state = AccountState.under_audit;
+                    return true;
+                case "frozen":
+                    state = AccountState.frozen;
+                    return true;
+                case "closed":
+                    state = AccountState.closed;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Get account state
+        /// </summary>
+        /// <returns>enum</returns>
+        public Enum GetState()
+        {
+            return state;
+        }
+
+    }
+
+    /// <summary>
+    /// Interface for Account class
+    /// used for data separation/protection
+    /// </summary>
+    public interface IAccount
+    {
+        bool SetName(string inName);
+        string GetName();
+
+        bool SetAddress(string inAddress);
+        string GetAddress();
+
+        bool PayInFunds(decimal amount);
+        bool WithdrawFunds(decimal amount);
+
+        bool SetBalance(decimal inBalance);
+        decimal GetBalance();
+
+        bool SetAccountNumber(int inAccountNumber);
+        int GetAccountNumber();
+
+        bool SetState(string inState);
+        Enum GetState();
+
+    }
+}
+
---
+++ Lab02/Program.cs
@@ -0,0 +1,108 @@
+﻿// Project Prolog
+// Name: Andrew Pritchett
+// CS3260 Section X01
+// Project: Lab_02
+// Date: 07/03/2022
+// Purpose: Create a bank account class and an IAccount interface
+// to access it (practicing data hiding/loose coupling). Test all
+// functionality.
+//
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes plagiarism, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+
+namespace Lab02
+{
+    /// <summary>
+    /// Main driver for the project.
+    /// </summary>
+    public class Lab02
+    {
+        /// <summary>
+        /// Main testing function - instantiate Account through
+        /// IAccount interface and test all functionality.
+        /// </summary>
+        public static void Main()
+        {
+            // Instantiate account through interface
+            IAccount account = new Account();
+
+            // test base state
+            Console.WriteLine("Test base state \"new_account\": ");
+            Console.WriteLine("Account State = " + account.GetState());
+
+            // set basic account details
+            Console.WriteLine("Setting account number...");
+            Console.WriteLine(account.SetAccountNumber(1001));
+            Console.WriteLine("Setting name...");
+            Console.WriteLine(account.SetName("Andrew Pritchett"));
+            Console.WriteLine("Setting address...");
+            Console.WriteLine(account.SetAddress("10412 N. 6300 W. Highland, UT 84003"));
+            Console.WriteLine("Setting balance....");
+            Console.WriteLine(account.SetBalance(100.00m));
+
+            // print the basic account details
+            Console.WriteLine("Account Number = " + account.GetAccountNumber());
+            Console.WriteLine("Name = " + account.GetName());
+            Console.WriteLine("Address = " + account.GetAddress());
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test bad name input
+            Console.WriteLine("Test bad name set:");
+            Console.WriteLine(account.SetName(""));
+
+            // test bad address input
+            Console.WriteLine("Test bad address set:");
+            Console.WriteLine(account.SetAddress(""));
+
+            // hold the good states + 1 bad state for testing
+            string[] states = { "new_account", "active", "under_audit", "frozen", "closed", "bad_state" };
+
+            // saved some code by iterating through each state
+            foreach (string st in states)
+            {
+                Console.WriteLine($"Test state \"{st}\": "); // state to test
+                Console.WriteLine(account.SetState(st)); // set state, print true or false
+                Console.WriteLine("Account State = " + account.GetState()); // output current state
+            }
+
+            // test bad balance set
+            Console.WriteLine("Setting balance at < minimum @ $90.00:");
+            Console.WriteLine(account.SetBalance(90.00m));
+
+            // test good balance set
+            Console.WriteLine("Setting balance at > minimum @ $200.00:");
+            Console.WriteLine(account.SetBalance(200.00m));
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test good deposit
+            Console.WriteLine("Deposit $1000");
+            Console.WriteLine(account.PayInFunds(1000m));
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test bad deposit
+            Console.WriteLine("Deposit -$10");
+            Console.WriteLine(account.PayInFunds(-10m));
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test good withdraw
+            Console.WriteLine("Withdraw $75");
+            account.WithdrawFunds(75m);
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test bad withdraw
+            Console.WriteLine("Attempt overdraft: ");
+            Console.WriteLine(account.WithdrawFunds(10000m));
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+            // test withdraw to empty
+            Console.WriteLine("Withdraw to 0: ");
+            Console.WriteLine(account.WithdrawFunds(account.GetBalance()));
+            Console.WriteLine("Balance = " + account.GetBalance());
+
+        }
+    }
+}
